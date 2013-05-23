2b1bc95674076f3cf41b67f0ae7186e9ab108333
Gabor de Mooij <gabordemooij@gmail.com>
2013-05-23 18:08:04 +0200

    Update version string.

--- RedBean/Facade.php
+++ RedBean/Facade.php
@@ -3,7 +3,7 @@
  * RedBean Facade
  * 
  * Version Information
- * RedBean Version @version 3.4.4
+ * RedBean Version @version 3.4.6
  * 
  * @file			RedBean/Facade.php
  * @desc			Convenience class for RedBeanPHP.
@@ -80,7 +80,7 @@ class RedBean_Facade {
 	 * @return string
 	 */
 	public static function getVersion() {
-		return '3.4.5';
+		return '3.4.6';
 	}
 	/**
 	 * Kickstarts redbean for you. This method should be called before you start using
